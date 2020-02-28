5556fdccde0f0a41f529fd483cbe2008d5eb7a12
AJ Larson <ajlars@hotmail.com>
2020-02-27 22:49:23 -0700

    updated for simpler setup

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,16 +1,19 @@
 module.exports = {
-  "src_folders" : ["tests"],
-  "page_objects_path" : 'pageObjects',
-  "webdriver" : {
-    "start_process": true,
-    "server_path": require("chromedriver").path,
-    "port": 9515
+  src_folders : ["tests"],
+  page_objects_path : "pageObjects",
+  webdriver : {
+    start_process: true,
+    server_path: require("chromedriver").path,
+    port: 9515
   },
 
-  "test_settings" : {
-    "default" : {
-      "desiredCapabilities": {
-        "browserName": "chrome"
+  test_settings : {
+    default : {
+      desiredCapabilities: {
+        browserName: "chrome",
+        chromeOptions:{
+          w3c:false
+        }
       }
     }
   }
