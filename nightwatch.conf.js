87853cd3892f1c60bf8aac4bd1d5dffb875ea62b
AJ Larson <ajlars@hotmail.com>
2020-02-27 22:44:20 -0700

    updated for simpler setup

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,43 +1,17 @@
-const properties = require('./nightwatch.props')
 module.exports = {
-    "src_folders" : "tests",
-    "page_objects_path" : "pageObjects",  
-    "selenium" : {
-      "start_process" : true,
-      "server_path" : properties.resourcePath + properties.seleniumServer,
-      "log_path" : "",
-      "port" : 4445,
-      "cli_args" : {
-        "webdriver.chrome.driver" : properties.resourcePath + properties.chromedriver,
-      }
-    },
-  
-    "test_settings" : {
-      "default" : {
-        "launch_url" : "http://localhost",
-        "selenium_port"  : 4445,
-        "selenium_host"  : "localhost",
-        "silent": true,
-        "screenshots" : {
-          "enabled" : false,
-          "path" : ""
-        },
-        "desiredCapabilities": {
-          "browserName": "chrome",
-        }
-      },
-  
-      "firefox" : {
-        "desiredCapabilities": {
-          "browserName": "firefox",
-          "marionette": true
-        }
-      },
-  
-      "edge" : {
-        "desiredCapabilities": {
-          "browserName": "MicrosoftEdge"
-        }
+  "src_folders" : ["tests"],
+  "page_objects_path" : 'pageObjects',
+  "webdriver" : {
+    "start_process": true,
+    "server_path": require("chromedriver").path,
+    "port": 9515
+  },
+
+  "test_settings" : {
+    "default" : {
+      "desiredCapabilities": {
+        "browserName": "chrome"
       }
     }
   }
+}
